104ef90c7a250b803d274be546eedc9927af4cc3
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2021-01-22 20:24:34 -0300

    Alter app module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import RepoModule from './repo.module';
+import { AppService } from './app.service';
 
 import ormOptions from './config/orm';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import UserResolver from './resolvers/user.resolver';
+import MessageResolver from './resolvers/message.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+
+const gqlImports = [UserResolver, MessageResolver];
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormOptions), RepoModule],
+  imports: [
+    TypeOrmModule.forRoot(ormOptions),
+    RepoModule,
+    ...gqlImports,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+      installSubscriptionHandlers: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
