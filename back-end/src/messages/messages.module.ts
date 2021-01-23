84240f418bd85efb2e46fa68e4ca55126aea9946
Erick Malta <erickmalta100@gmail.com>
2021-01-23 10:52:47 -0300

    feat(messages): provide message gateway

--- back-end/src/messages/messages.module.ts
+++ back-end/src/messages/messages.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
 import Message from './entities/message.entity';
+import { MessagesGateway } from './messages.gateway';
 import MessageResolver from './resolvers/message.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Message]), UsersModule],
-  providers: [MessageResolver],
+  providers: [MessagesGateway, MessageResolver],
   exports: [MessageResolver],
 })
 export class MessagesModule {}
