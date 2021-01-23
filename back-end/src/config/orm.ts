8810fc30c54ebd886c7f06ec1797c4c8ea714ca8
Erick Malta <erickmalta100@gmail.com>
2021-01-23 01:15:56 -0300

    feat(config): add new postgres orm configuration

--- back-end/src/config/orm.ts
+++ back-end/src/config/orm.ts
@@ -4,9 +4,12 @@ import * as path from 'path';
 //Seguir os passos do link tutorial
 
 const options: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: 'data/uniChat.db',
-  logging: true,
+  type: 'postgres',
+  host: 'postgres',
+  port: 5432,
+  username: 'postgres',
+  password: 'unichat',
+  database: 'unichat',
   entities: [
     path.resolve(__dirname, '..', '**', 'entities', '*.entity.{js,ts}'),
   ],
