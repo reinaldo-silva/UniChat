0da62c462b1d116ac9d60e842ce36ef103a8d198
Erick Malta <erickmalta100@gmail.com>
2021-01-23 10:52:25 -0300

    feat(messages): add message socket gateway

---
+++ back-end/src/messages/messages.gateway.ts
@@ -0,0 +1,49 @@
+import { Injectable, Logger, UsePipes, ValidationPipe } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { Repository } from 'typeorm';
+import { CreateMessageDTO } from './dtos/create-message.dto';
+import Message from './entities/message.entity';
+
+@Injectable()
+@WebSocketGateway({ transports: ['websocket'] })
+export class MessagesGateway
+  implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    @InjectRepository(Message)
+    private readonly messagesRepository: Repository<Message>,
+    private readonly logger: Logger,
+  ) {
+    this.logger = new Logger(MessagesGateway.name);
+  }
+
+  @SubscribeMessage('message')
+  @UsePipes(ValidationPipe)
+  async handleMessage(
+    @MessageBody() createMessageDTO: CreateMessageDTO,
+    @ConnectedSocket() client: Socket,
+  ): Promise<Message> {
+    const createdMessage = this.messagesRepository.create(createMessageDTO);
+
+    await this.messagesRepository.save(createdMessage);
+
+    client.emit('get-message', { message: createdMessage });
+    return createdMessage;
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.verbose(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.verbose(`Client disconnected: ${client.id}`);
+  }
+}
